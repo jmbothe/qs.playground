f143b4e7e05e6cae053c5d72f9e3356520a813be
jmbothe <jeffrymbothe@gmail.com>
2018-10-09 21:50:31 -0400

    add styles and header/heading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -101,21 +101,54 @@ class App extends Component {
   render() {
     return (
       <Provider value={this.state}>
+        <header>
+          <h1>qs Playground</h1>
+        </header>
         <Container>
+          <p>
+            qs is a JavaScript library that stringifies nested object literals
+            into url-encoded strings, perfect for sending as query strings in
+            HTTP requests. It can also parse such strings back into objects.
+            This is just a simple little playground where you can see qs at
+            work. This playground might be useful to you if you want to quickly
+            generate some qs query strings for prototyping or testing purposes.
+          </p>
+          <p>
+            Express.js uses qs under the hood. So if your backend is an
+            Express.js app, you can us qs on the front end to create query
+            strings (insert your qs query string immediately after the "?" in
+            your request url), and when the request arrives at your backend
+            server the query string will be parsed automatically.
+          </p>
+          <p>
+            Find the full qs documentation
+{" "}
+            <a
+              href="https://github.com/ljharb/qs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+            .
+          </p>
+
           <Form>
             <Form.TextArea
+              rows="8"
               label="JavaScript Object Literal Input"
               onChange={this.handleChange('objectForm', this.parseObjectInput)}
             />
-            <Message>
+            <Message negative={!this.state.objectForm.isValid}>
               <Message.Header>Stringified Output</Message.Header>
               {this.state.objectForm.outputValue}
             </Message>
             <Form.TextArea
+              rows="8"
               label="qs String Input"
               onChange={this.handleChange('stringForm', this.parseStringInput)}
             />
-            <Message>
+            <Message negative={!this.state.stringForm.isValid}>
               <Message.Header>Object Literal Output</Message.Header>
               {this.state.stringForm.outputValue}
             </Message>
